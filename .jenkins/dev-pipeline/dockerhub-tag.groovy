9cea570563f2ef4d1688b3e8c668f6d890762343
Attila Palfi <apalfi@hortonworks.com>
2018-09-24 13:34:12 +0200

    BUG-111336 - added missing VERSION parameter

--- .jenkins/dev-pipeline/dockerhub-tag.groovy
+++ .jenkins/dev-pipeline/dockerhub-tag.groovy
@@ -1,6 +1,10 @@
 pipeline {
     agent any
 
+    parameters {
+        string(name: 'VERSION', defaultValue: '', description: 'Cloudbreak version to build the Docker containers from. Example: 1.5.0-dev.87.')
+    }
+
     stages {
         stage('Tag Cloudbreak-autoscale') {
             environment {
@@ -9,7 +13,7 @@ pipeline {
             }
             steps {
                 script {
-                    currentBuild.displayName = "${BUILD_NUMBER}-${VERSION}"
+                    currentBuild.displayName = "${BUILD_NUMBER}-${params.VERSION}"
                 }
                 sh '''
                     make dockerhub
