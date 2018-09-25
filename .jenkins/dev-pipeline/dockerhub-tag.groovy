857e8cb0ac307b593c8a1f305f9ecb2b702a4ecc
Attila Palfi <apalfi@hortonworks.com>
2018-09-25 16:59:03 +0200

    BUG-111336 - moving environment block

--- .jenkins/dev-pipeline/dockerhub-tag.groovy
+++ .jenkins/dev-pipeline/dockerhub-tag.groovy
@@ -5,12 +5,13 @@ pipeline {
         string(name: 'VERSION', defaultValue: '', description: 'Cloudbreak version to build the Docker containers from. Example: 1.5.0-dev.87.')
     }
 
+    environment {
+        DOCKERHUB_USERNAME = credentials('dockerhub-username')
+        DOCKERHUB_PASSWORD = credentials('dockerhub-password')
+    }
+
     stages {
         stage('Tag Cloudbreak-autoscale') {
-            environment {
-                DOCKERHUB_USERNAME = credentials('dockerhub-username')
-                DOCKERHUB_PASSWORD = credentials('dockerhub-password')
-            }
             steps {
                 script {
                     currentBuild.displayName = "${BUILD_NUMBER}-${params.VERSION}"
